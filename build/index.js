ca2017141301503d58134cc48d985747feb87cff
Muhammad Fawky <mfawky07@gmail.com>
2022-05-20 15:18:08 +0200

    Writing some tests with jasmine

--- build/index.js
+++ build/index.js
@@ -1,20 +1,47 @@
-'use strict'
-const __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { 'default': mod }
-}
-Object.defineProperty(exports, '__esModule', { value: true })
-const express_1 = __importDefault(require('express'))
-const PORT = process.env.PORT || 3000
-// create an instance server
-const app = (0, express_1.default)()
-// add routing for / path
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World '
-    })
-})
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const sharp_1 = __importDefault(require("sharp"));
+const app = (0, express_1.default)();
+app.get('/api/images', (req, res) => {
+    const fileName = req.query.fileName;
+    const width = req.query.width;
+    const height = req.query.height;
+    if (!fileName || !width || !height) {
+        //throw exception
+        console.log('Mandatory input is missing');
+        res.status(400);
+        res.send('Mandatory input is missing');
+        return;
+    }
+    const thumbPath = path_1.default.resolve(`./assets/thumb/${fileName}_thumb_${width}_${height}.jpg`);
+    const fullPath = path_1.default.resolve(`./assets/full/${fileName}.jpg`);
+    if (fs_1.default.existsSync(thumbPath)) {
+        console.log('found as thumb');
+        res.sendFile(thumbPath);
+    }
+    else {
+        console.log('resize full image');
+        if (!fs_1.default.existsSync(fullPath)) {
+            console.log('File not found');
+            res.send('File not found');
+            return;
+        }
+        (0, sharp_1.default)(fullPath)
+            .resize(+width, +height)
+            .toFile(thumbPath, function (err) {
+            if (!err)
+                res.sendFile(thumbPath);
+        });
+    }
+});
 // start express server
-app.listen(PORT, () => {
-    console.log(`Server is starting at prot:${PORT}`)
-})
-exports.default = app
+app.listen(3000, () => {
+    console.log('Server is starting at port: 3000');
+});
+exports.default = app;
--- build/tests/index.spec.js
+++ build/tests/index.spec.js
@@ -1,1 +1,39 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const supertest_1 = __importDefault(require("supertest"));
+const index_1 = __importDefault(require("../index"));
+const request = (0, supertest_1.default)(index_1.default);
+describe('Test endpoint responses', () => {
+    it('expects valid query strings', () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield request.get(`/api/images?fileName=aaa&width=123&height=456`);
+        expect(response.status).toBe(200);
+    }));
+    it('expects query without height', () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield request.get(`/api/images?fileName=aaa&width=123`);
+        expect(response.status).toBe(400);
+    }));
+    it('expects query without width', () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield request.get(`/api/images?fileName=aaa&height=321`);
+        expect(response.status).toBe(400);
+    }));
+    it('expects query without fileName', () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield request.get(`/api/images?&width=123&height=321`);
+        expect(response.status).toBe(400);
+    }));
+    it('expects query with valid params and invalid endpoint string', () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield request.get(`//images?&width=123&height=321`);
+        expect(response.status).toBe(404);
+    }));
+});
